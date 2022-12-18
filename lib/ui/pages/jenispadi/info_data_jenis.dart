8155245338aa2f6330cbccf6c6c7b09bc535fcaa
Lusyyy <lusyyd1@gmail.com>
2022-12-18 21:03:30 +0700

    jenis

--- lib/ui/pages/jenispadi/info_data_jenis.dart
+++ lib/ui/pages/jenispadi/info_data_jenis.dart
@@ -1,191 +1,192 @@
-// import 'package:Edifarm/API/Api_service.dart';
-// import 'package:Edifarm/models/Jenis_Model.dart';
-// import 'package:Edifarm/models/Lahan_model.dart';
-// import 'package:Edifarm/shared/Theme_App.dart';
+import 'package:Edifarm/shared/Theme_App.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/animation/animation_controller.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/ticker_provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-// import 'package:flutter/material.dart';
+class InfoLink extends StatefulWidget {
+  const InfoLink({super.key});
 
-// class DataJenis extends StatefulWidget {
-//   const DataJenis({Key? key}) : super(key: key);
+  @override
+  State<InfoLink> createState() => _InfoLinkState();
+}
 
-//   @override
-//   State<DataJenis> createState() => _DataJenisState();
-// }
+class _InfoLinkState extends State<InfoLink>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
 
-// class _DataJenisState extends State<DataJenis> {
-//   late Future listdata;
-//   List<JenisModel> listdata1 = [];
-//   @override
-//   void initState() {
-//     super.initState();
-//     listdata = ServiceApiJenis().getData();
-//     listdata.then((value) {
-//       setState(() {
-//         listdata1 = value;
-//       });
-//     });
-//   }
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
 
-//   @override
-//   void dispose() {
-//     super.dispose();
-//   }
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//         height: 100,
-//         child: listdata1.length == 0
-//             ? Center(
-//                 child: CircularProgressIndicator(color: AppTheme.orange),
-//               )
-//             : ListView.builder(
-//                 itemCount: listdata1.length,
-//                 itemBuilder: (context, index) {
-//                   return Row(
-//                     children: [
-//                       Container(
-//                           height: 60,
-//                           width: 100,
-//                           decoration: BoxDecoration(
-//                               // borderRadius: BorderRadius.circular(10),
-//                               color: AppTheme.nearlyWhite,
-//                               borderRadius:
-//                                   const BorderRadius.all(Radius.circular(16.0)),
-//                               boxShadow: <BoxShadow>[
-//                                 BoxShadow(
-//                                     color: AppTheme.grey.withOpacity(0.2),
-//                                     offset: const Offset(1.1, 1.1),
-//                                     blurRadius: 8.0),
-//                               ]),
-//                           child: Padding(
-//                             padding: const EdgeInsets.only(
-//                                 left: 10.0, right: 10.0, top: 4.0, bottom: 4.0),
-//                             child: Column(
-//                               mainAxisAlignment: MainAxisAlignment.center,
-//                               crossAxisAlignment: CrossAxisAlignment.center,
-//                               children: <Widget>[
-//                                 Text(
-//                                   'Pemilik',
-//                                   textAlign: TextAlign.center,
-//                                   style: const TextStyle(
-//                                     fontWeight: FontWeight.w600,
-//                                     fontFamily: AppTheme.fontName,
-//                                     fontSize: 14,
-//                                     letterSpacing: 0.27,
-//                                     color: AppTheme.green,
-//                                   ),
-//                                 ),
-//                                 Text(
-//                                   listdata1[index].!,
-//                                   textAlign: TextAlign.center,
-//                                   style: const TextStyle(
-//                                     fontWeight: FontWeight.w200,
-//                                     fontFamily: AppTheme.fontName,
-//                                     fontSize: 14,
-//                                     letterSpacing: 0.27,
-//                                     color: AppTheme.grey,
-//                                   ),
-//                                 ),
-//                               ],
-//                             ),
-//                           )),
-//                       const Spacer(),
-//                       Container(
-//                           height: 60,
-//                           width: 100,
-//                           decoration: BoxDecoration(
-//                               // borderRadius: BorderRadius.circular(10),
-//                               color: AppTheme.nearlyWhite,
-//                               borderRadius:
-//                                   const BorderRadius.all(Radius.circular(16.0)),
-//                               boxShadow: <BoxShadow>[
-//                                 BoxShadow(
-//                                     color: AppTheme.grey.withOpacity(0.2),
-//                                     offset: const Offset(1.1, 1.1),
-//                                     blurRadius: 8.0),
-//                               ]),
-//                           child: Padding(
-//                             padding: const EdgeInsets.only(
-//                                 left: 10.0, right: 10.0, top: 4.0, bottom: 4.0),
-//                             child: Column(
-//                               mainAxisAlignment: MainAxisAlignment.center,
-//                               crossAxisAlignment: CrossAxisAlignment.center,
-//                               children: <Widget>[
-//                                 Text(
-//                                   'Luas',
-//                                   textAlign: TextAlign.center,
-//                                   style: const TextStyle(
-//                                     fontWeight: FontWeight.w600,
-//                                     fontFamily: AppTheme.fontName,
-//                                     fontSize: 14,
-//                                     letterSpacing: 0.27,
-//                                     color: AppTheme.green,
-//                                   ),
-//                                 ),
-//                                 Text(
-//                                   listdata1[index].luasLahan!,
-//                                   textAlign: TextAlign.center,
-//                                   style: const TextStyle(
-//                                     fontWeight: FontWeight.w200,
-//                                     fontFamily: AppTheme.fontName,
-//                                     fontSize: 14,
-//                                     letterSpacing: 0.27,
-//                                     color: AppTheme.grey,
-//                                   ),
-//                                 ),
-//                               ],
-//                             ),
-//                           )),
-//                       const Spacer(),
-//                       Container(
-//                           height: 60,
-//                           width: 100,
-//                           decoration: BoxDecoration(
-//                               // borderRadius: BorderRadius.circular(10),
-//                               color: AppTheme.nearlyWhite,
-//                               borderRadius:
-//                                   const BorderRadius.all(Radius.circular(16.0)),
-//                               boxShadow: <BoxShadow>[
-//                                 BoxShadow(
-//                                     color: AppTheme.grey.withOpacity(0.2),
-//                                     offset: const Offset(1.1, 1.1),
-//                                     blurRadius: 8.0),
-//                               ]),
-//                           child: Padding(
-//                             padding: const EdgeInsets.only(
-//                                 left: 10.0, right: 10.0, top: 4.0, bottom: 4.0),
-//                             child: Column(
-//                               mainAxisAlignment: MainAxisAlignment.center,
-//                               crossAxisAlignment: CrossAxisAlignment.center,
-//                               children: <Widget>[
-//                                 Text(
-//                                   'Tempat',
-//                                   textAlign: TextAlign.center,
-//                                   style: const TextStyle(
-//                                     fontWeight: FontWeight.w600,
-//                                     fontFamily: AppTheme.fontName,
-//                                     fontSize: 14,
-//                                     letterSpacing: 0.27,
-//                                     color: AppTheme.green,
-//                                   ),
-//                                 ),
-//                                 Text(
-//                                   listdata1[index].tempat!,
-//                                   textAlign: TextAlign.center,
-//                                   style: const TextStyle(
-//                                     fontWeight: FontWeight.w200,
-//                                     fontFamily: AppTheme.fontName,
-//                                     fontSize: 14,
-//                                     letterSpacing: 0.27,
-//                                     color: AppTheme.grey,
-//                                   ),
-//                                 ),
-//                               ],
-//                             ),
-//                           ))
-//                     ],
-//                   );
-//                 }));
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(children: [
+        InkWell(
+          onTap: () {
+            launch("https://wa.me/+6289503415644");
+          },
+          child: Card(
+            child: Container(
+              height: 90,
+              width: 90,
+              decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage('icon_jenis_kisaran.png'),
+                    opacity: 20,
+                  ),
+                  color: AppTheme.nearlyWhite,
+                  borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: AppTheme.grey.withOpacity(0.2),
+                        offset: const Offset(1.1, 1.1),
+                        blurRadius: 8.0),
+                  ]),
+            ),
+          ),
+        ),
+        const Spacer(),
+        InkWell(
+          onTap: () {
+            launch("https://wa.me/+6289503415644");
+          },
+          child: Card(
+            child: Container(
+              height: 90,
+              width: 90,
+              decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage(''),
+                    opacity: 20,
+                  ),
+                  color: AppTheme.nearlyWhite,
+                  borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: AppTheme.grey.withOpacity(0.2),
+                        offset: const Offset(1.1, 1.1),
+                        blurRadius: 8.0),
+                  ]),
+            ),
+          ),
+        ),
+        const Spacer(),
+        InkWell(
+          onTap: () {
+            launch("https://wa.me/+6289503415644");
+          },
+          child: Card(
+            child: Container(
+              height: 90,
+              width: 90,
+              decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage(''),
+                    opacity: 20,
+                  ),
+                  color: AppTheme.nearlyWhite,
+                  borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: AppTheme.grey.withOpacity(0.2),
+                        offset: const Offset(1.1, 1.1),
+                        blurRadius: 8.0),
+                  ]),
+            ),
+          ),
+        ),
+      ]),
+      Row(
+        children: [
+          InkWell(
+            onTap: () {
+              launch("https://wa.me/+6289503415644");
+            },
+            child: Card(
+              child: Container(
+                height: 90,
+                width: 90,
+                decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage(''),
+                      opacity: 20,
+                    ),
+                    color: AppTheme.nearlyWhite,
+                    borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          color: AppTheme.grey.withOpacity(0.2),
+                          offset: const Offset(1.1, 1.1),
+                          blurRadius: 8.0),
+                    ]),
+              ),
+            ),
+          ),
+          const Spacer(),
+          InkWell(
+            onTap: () {
+              launch("https://wa.me/+6289503415644");
+            },
+            child: Card(
+              child: Container(
+                height: 90,
+                width: 90,
+                decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage(''),
+                      opacity: 20,
+                    ),
+                    color: AppTheme.nearlyWhite,
+                    borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          color: AppTheme.grey.withOpacity(0.2),
+                          offset: const Offset(1.1, 1.1),
+                          blurRadius: 8.0),
+                    ]),
+              ),
+            ),
+          ),
+          const Spacer(),
+          InkWell(
+            onTap: () {
+              launch("https://wa.me/+6289503415644");
+            },
+            child: Card(
+              child: Container(
+                height: 90,
+                width: 90,
+                decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage(''),
+                      opacity: 20,
+                    ),
+                    color: AppTheme.nearlyWhite,
+                    borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          color: AppTheme.grey.withOpacity(0.2),
+                          offset: const Offset(1.1, 1.1),
+                          blurRadius: 8.0),
+                    ]),
+              ),
+            ),
+          ),
+        ],
+      )
+    ]);
+  }
+}
